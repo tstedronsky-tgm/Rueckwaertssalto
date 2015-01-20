f6062ed53064611315622488bc210c22cf8206e6
Thomas Stedronsky <tstedronsky@student.tgm.ac.at>
2015-01-20 13:15:22 +0100

    FK funktionieren und Zeitaufzeichnung

--- src/anil_stedronsky/Model.java
+++ src/anil_stedronsky/Model.java
@@ -13,7 +13,14 @@ public class Model {
 	private MysqlDataSource ds;
 	private Connection con;
 	private DatabaseMetaData db;
-
+	
+	/**
+	 * Konstruktor der host, user, pw und database einliest
+	 * @param die Hostadresse
+	 * @param Der DB User
+	 * @param Das PW für die DB
+	 * @param Die DB die angesprochen werden soll. 
+	 */
 	public Model(String host, String user, String pw, String database){
 		this.ds= new MysqlDataSource();
 		ds.setServerName(host);
@@ -37,11 +44,11 @@ public class Model {
 		String[] tables=null;
 		try {
 			String[] types = {"TABLE"};
-			ResultSet rs = db.getTables(null, null, "%", types);
+			ResultSet rs = db.getTables(null, null, "%", types); //ResultSet für die Tabellennamen
 			while(rs.next()){
-				al.add(rs.getString("TABLE_NAME"));
+				al.add(rs.getString("TABLE_NAME")); //Tabellenname wird ausgelesen
 			}
-			tables=al.toArray(new String[al.size()]);
+			tables=al.toArray(new String[al.size()]); 
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -54,32 +61,24 @@ public class Model {
 	 * @return Array mit den Tables
 	 */
 	public String[] getAtt(String tabname) {
-		ArrayList<String> pk = new ArrayList<String>();
-		ResultSet pks;
-		try {
-			pks = db.getPrimaryKeys(null, null, tabname);
-			while (pks.next()) {
-				pk.add(pks.getString("COLUMN_NAME"));
-			}
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-		}
-		String[] prk=pk.toArray(new String[pk.size()]);
-		
-		
-
+		String[] prk=getPK(tabname);
+		String[] frk=getFK(tabname);
 		ArrayList<String> al= new ArrayList<String>();
 		String[] att = null;
 		try {
-			ResultSet rs = db.getColumns(null, null, tabname,null);
+			ResultSet rs = db.getColumns(null, null, tabname,null); //ResultSet für die Attributnamen
 			while(rs.next()){
-				al.add(rs.getString("COLUMN_NAME"));
+				al.add(rs.getString("COLUMN_NAME")); //Die Attributte werden ausgelesen
+			}
+			for(int i=0; i<frk.length;++i){
+				al.add(frk[i]);
 			}
 			att = al.toArray(new String[al.size()]);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		
+		/*Schleife die den Attributen den PK hinzufügt*/
 		for(int i=0; i<att.length;++i){
 			for(int j=0; j<prk.length;++j){
 				if(att[i].equals(prk[j])){
@@ -91,6 +90,54 @@ public class Model {
 
 	}
 	
+	/**
+	 * Liefert den/die PKs zurück 
+	 * @param Die Tabelle die auf PK überprüft werden soll
+	 * @return die PK
+	 */
+	public String[] getPK(String tabname){
+		ArrayList<String> pk = new ArrayList<String>();
+		ResultSet pks;
+		try {
+			pks = db.getPrimaryKeys(null, null, tabname);  //ResultSet für den PK
+			while (pks.next()) {
+				pk.add(pks.getString("COLUMN_NAME"));  //PK in einer bestimmten Tabelle auslesen
+			}
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+		String[] prk=pk.toArray(new String[pk.size()]);
+		
+		return prk;
+	}
+	
+	/**
+	 * Liefert den/die FKs zurück 
+	 * @param Die Tabelle die auf PK überprüft werden soll
+	 * @return die PK
+	 */
+	public String[] getFK(String tabname){
+		ArrayList<String> fk = new ArrayList<String>();
+		ResultSet fks;
+		try {
+			fks = db.getImportedKeys(null, null, tabname);
+			while (fks.next()) {
+				
+				//Den FK mit den Relations
+				fk.add(fks.getString("FKCOLUMN_NAME")+":"+fks.getString("PKTABLE_NAME")+"."+fks.getString("PKCOLUMN_NAME"));
+			}
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+		String[] frk=fk.toArray(new String[fk.size()]);
+		
+		return frk;
+	}
+	
+	/**
+	 * Liefert das fertige RM zurück
+	 * @return das fertige RM
+	 */
 	public String getRM() {
 		String rm="";
 		String[] tables = getTables();
--- src/anil_stedronsky/Start.java
+++ src/anil_stedronsky/Start.java
@@ -9,11 +9,14 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
-
+/**
+ * RM wird gestartet 
+ * @author Stedronsky Thomas
+ * @version 2015-01-20
+ */
 public class Start {
 	public static void main(String[] args) {
-		Model m = new Model("localhost", "rueck", "fener", "schokoladenfabrik");
-		
+		Model m = new Model("localhost", "rueck", "fener", "schokoladenfabrik"); //DB Connection Parameter werden an das Model übergeben
 		System.out.println(m.getRM());
 		m.close();
 	}
